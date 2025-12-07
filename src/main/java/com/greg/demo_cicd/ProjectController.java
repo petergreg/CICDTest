d58f7e79294c09d3a7ff80a5d4018189b2f41347
Gregoire <gregoire.peterschmitt@gmail.com>
2025-12-07 19:31:24 +0100

    testing push to github

--- src/main/java/com/greg/demo_cicd/ProjectController.java
+++ src/main/java/com/greg/demo_cicd/ProjectController.java
@@ -9,6 +9,6 @@ public class ProjectController {
 
     @GetMapping("/hello")
     public String helloGreg() {
-        return "Hello Paris";
+        return "Hello Paris test new SSH key";
     }
 }
